627df0bb38e2f48e90d367fcce24b882bed07955
Dario Bruckner TGM <dbruckner01@student.tgm.ac.at>
2018-06-12 18:27:07 +0200

    SimpleChatClient total changed
    SimpleChatClient totally changed to make it  have the GUI in it

--- SimpleChatJava/src/BrucknerD/SimpleChatClient.java
+++ SimpleChatJava/src/BrucknerD/SimpleChatClient.java
@@ -1,5 +1,16 @@
 package BrucknerD;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,41 +19,98 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.*;
 
-import javax.swing.JFrame;
 
 /**
  * 
  * @author Dario
  *
- *	Main Klasse für Client. Startet GUI und Funktionen
+ *	Main Klasse fuer Client. Startet GUI und Funktionen
  *
  */
 
 
 
-public class SimpleChatClient{
-	String input = null;
+public class SimpleChatClient extends Application {
+
+    String input = null;
 	PrintWriter writer;
 	OutputStream out;
 	Socket client;
+	SimpleChatClient scl;
+
+    private Button send;
+    private TextField ginput,gclient;
+    private TextArea output;
+    private ChatClientSocket cl;
 	
 	public static void main(String [] args){
 		
 		//Startet einen Client
-		ChatClientSocket  a = new ChatClientSocket(1);
+        launch(args);
+
+
 	}
-	
+
+    @Override
+    public void start(Stage clientpage) throws Exception {
+
+	    startclient();
+	    clientpage.setTitle("client");
+
+	    scl = this;
+
+        cl = new ChatClientSocket(scl);
+
+        send = new Button("Senden");
+        send.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                cl.sendtoserver(ginput.getText(),gclient.getText());
+                gclient.setEditable(false);
+            }
+        });
+        ginput = new TextField();
+        gclient = new TextField();
+        output = new TextArea();
+
+        FlowPane layout = new FlowPane();
+
+        output.setMinSize(610, 330);
+        output.setEditable(false);
+
+        gclient.setMinWidth(70);
+        ginput.setMinWidth(320);
+        send.setMinWidth(100);
+
+        layout.setPadding(new Insets(20));
+        layout.setVgap(20);
+        layout.setHgap(20);
+
+
+        layout.getChildren().add(output);
+        layout.getChildren().add(gclient);
+        layout.getChildren().add(ginput);
+        layout.getChildren().add(send);
+
+        Scene scene = new Scene(layout, 650,420);
+        clientpage.setScene(scene);
+        clientpage.show();
+
+    }
+
+
+
 	/**
 	 * 
 	 * @author Dario
 	 * 
-	 * Öffnet einen Socket und setzt den outputstream
+	 * oeffnet einen Socket und setzt den outputstream
 	 * 
 	 * 
 	 */
 	
 	
-	public SimpleChatClient(){
+	public void startclient(){
 		try {
 			
 			//Client
@@ -56,7 +124,9 @@ public class SimpleChatClient{
 			
 			e.printStackTrace();
 		}
-	}	
+	}
+
+
 	
 	/**
 	 * @author Dario
@@ -71,4 +141,14 @@ public class SimpleChatClient{
 		writer.flush();
 		
 	}
+    public void showClients(String Client){
+        String outputs = gclient.getText()+ Client + "\n";
+        gclient.setText(outputs);
+    }
+
+    
+    public void showtext(String input) {
+        String outputs = output.getText() + input + "\n";
+        output.setText(outputs);
+    }
 }
